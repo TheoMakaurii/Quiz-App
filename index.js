9e6f4ad0a5f7d38731dc2c176e53465a05c210ac
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2020-08-12 08:48:46 -0500

    reorganized our example files and the wireframe. Also made the store and setup functions we need to use

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-'use strict';
+/* eslint-disable no-undef, quotes, no-console */
 
+'use strict';
+/*
 const STORE = [
   {id: cuid(), name: "apples", checked: false},
   {id: cuid(), name: "oranges", checked: false},
   {id: cuid(), name: "milk", checked: true},
   {id: cuid(), name: "bread", checked: false}
 ];
+*/
 
+function generateQuestionsString(database){
+  // Here we will make the questions string to put in the form.
+}
+
+function generateShoppingItemsString(shoppingList) {
+  console.log("Generating shopping list element");
+  //console.log(shoppingList);
+  let questions = shoppingList.questions;
+  const items = questions.map((item) => generateItemElement(item));
+  
+  return items.join("");
+}
+
+function generateStartScreenString(database){
+  // Here we will return some similar to generateItemElement() function.
+  return "";
+}
+
+function generateQuestionScreenString(database,questions){
+  // Here we will return some similar to generateItemElement() function.
+  // Takes the value from generateQuestionsString and puts it in the form spot.
+  return "";
+}
+function generateResponseScreenString(database){
+  // Here we will return some similar to generateItemElement() function.
+  // Response will be two choices, if they get it right, if they get it wrong
+  // So do a check for that. 
+  return "";
+}
+function generateResultScreenString(database){
+  // Here we will return some similar to generateItemElement() function.
+  return "";
+}
 
 function generateItemElement(item) {
   return `
@@ -23,15 +59,28 @@ function generateItemElement(item) {
     </li>`;
 }
 
-
-function generateShoppingItemsString(shoppingList) {
-  console.log("Generating shopping list element");
-
-  const items = shoppingList.map((item) => generateItemElement(item));
-  
-  return items.join("");
+function generateStartScreen(){
+  // render the start screen in the DOM
+  // it will take the string from generateStartScreenString
+  // and put that in the dom.
+}
+function generateQuestionScreen(){
+  // render the Question screen in the DOM
+  // it will take the string from generateQuestionScreenString
+  // and put that in the dom.
 }
 
+function generateResponseScreen(){
+  // render the response screen in the DOM
+  // it will take the string from generateResponseScreenString
+  // and put that in the dom.
+}
+
+function generateResultsScreen(){
+  // render the result screen in the DOM
+  // it will take the string from generateResultsScreenString
+  // and put that in the dom.
+}
 
 function renderShoppingList() {
   // render the shopping list in the DOM
@@ -43,9 +92,26 @@ function renderShoppingList() {
 }
 
 
-function addItemToShoppingList(itemName) {
-  console.log(`Adding "${itemName}" to shopping list`);
-  STORE.push({id: cuid(), name: itemName, checked: false});
+
+// function addItemToShoppingList(itemName) {
+//   console.log(`Adding "${itemName}" to shopping list`);
+//   STORE.push({id: cuid(), name: itemName, checked: false});
+// }
+
+function handleQuizStart(){
+  //when the userStarts the quiz, render the first question. 
+}
+
+function handleAnswerSubmit(){
+  // takes the value from getValueFromCheckedAnswer function
+  // checks that value against the actual correct answer
+  // increment the currentQuestion counter
+  // changed the value of questionsRightOrWrong to either true or false
+  // then render the responseScreen
+}
+
+function handleNextQuestion(){
+  //If a user clicks the next question button, render the nextQuestionScreen();
 }
 
 function handleNewItemSubmit() {
@@ -59,12 +125,19 @@ function handleNewItemSubmit() {
   });
 }
 
-function toggleCheckedForListItem(itemId) {
-  console.log("Toggling checked property for item with id " + itemId);
-  const item = STORE.find(item => item.id === itemId);
-  item.checked = !item.checked;
-}
 
+// function toggleCheckedForListItem(itemId) {
+//   console.log("Toggling checked property for item with id " + itemId);
+//   const item = STORE.find(item => item.id === itemId);
+//   item.checked = !item.checked;
+// }
+
+
+function getValueFromCheckedAnswer(){
+  // Find the value of a "checked" radio button
+  // shound be something like this 
+  // $(`input[name=answers]:checked`).val();
+}
 
 function getItemIdFromElement(item) {
   return $(item)
@@ -72,34 +145,52 @@ function getItemIdFromElement(item) {
     .data('item-id');
 }
 
-function handleItemCheckClicked() {
-  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
-    console.log('`handleItemCheckClicked` ran');
-    const id = getItemIdFromElement(event.currentTarget);
-    toggleCheckedForListItem(id);
-    renderShoppingList();
-  });
-}
+
+// function handleItemCheckClicked() {
+//   $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+//     console.log('`handleItemCheckClicked` ran');
+//     const id = getItemIdFromElement(event.currentTarget);
+//     toggleCheckedForListItem(id);
+//     renderShoppingList();
+//   });
+// }
 
 
-function handleDeleteItemClicked() {
- $('.js-shopping-list').on('click', `.js-item-delete`, event =>{
+// function handleDeleteItemClicked() {
+//   $('.js-shopping-list').on('click', `.js-item-delete`, event =>{
+//     //console.log('you are clicking delete');
+//     const id = getItemIdFromElement(event.currentTarget);
+//     for(let i = 0; i < STORE.length; i++){
+//       //Looping through each item
+//       if(id === STORE[i].id){
+//         //If we find that item that matches the id, delete it. 
+//         console.log("We found the item!");
+//         //console.log(STORE[i]);
+//         console.log(i);
+//         STORE.splice(i, 1);
+//         console.log(STORE);
+//       }
+//     }
+//     renderShoppingList();
+//   });
 
-  console.log('`handleDeleteItemClicked` ran')
- })
+// }
 
-}
+
+console.log(STORE.questions[0]);
+console.log(STORE.responses[6]);
 
 // this function will be our callback when the page loads. it's responsible for
 // initially rendering the shopping list, and activating our individual functions
 // that handle new item submission and user clicks on the "check" and "delete" buttons
 // for individual shopping list items.
-function handleShoppingList() {
+function main() {
   renderShoppingList();
   handleNewItemSubmit();
-  handleItemCheckClicked();
-  handleDeleteItemClicked();
+  //handleItemCheckClicked();
+  //handleDeleteItemClicked();
+  
 }
 
 // when the page loads, call `handleShoppingList`
-$(handleShoppingList);
+$(main);
---
+++ store.js
@@ -0,0 +1,83 @@
+/* eslint-disable indent, no-undef, quotes*/
+'use strict';
+
+const STORE = {
+    currentQuestion: 0,
+    questionsCorrect: 0,
+    questionsRightOrWrong: false,
+    startScreen: {
+        title: `Do you have what it takes to be America's Next top Magician?`,
+        header: `Take this quiz and find out!`
+    },
+    questions: [
+        {
+            name: "In your opinion, which of these is the ideal pet?",
+            answers: ["Dog", "Cat", "Rabbit","Snake"],
+            correctAnswer: true
+        },
+        {
+            name: "Choose a color, ANY color.",
+            answers: ["Black", "Brown", "Periwinkle","White"],
+            correctAnswer: true
+        },
+        {
+            name: "Where do you keep your scarves?",
+            answers: ["In your closet.", "Around your neck.", "Up my sleeve.","You don't have a scarf."],
+            correctAnswer: true
+        },
+        {
+            name: "What is your magic weapon of choice?",
+            answers: ["Hammer", "Baseball Bat", "Wand","Sword"],
+            correctAnswer: true
+        },
+        {
+            name: "What are the suits in deck?",
+            answers: ["Ace, Spade, Club, Heart", "Ex, Oh, Ex, Oh", "Diamond, Spade, Club, Heart","Circle, Square, Triangle, Cross"],
+            correctAnswer: true
+        },
+        {
+            name: "Where are you most comfortable?",
+            answers: ["In Bed.", "In the streets.", "On stage","On a tropical island."],
+            correctAnswer: true
+        },
+        {
+            name: "What is your prestige?",
+            answers: ["Landing on the moon.", "Climbing Mount Everest", "Sawing a woman in half","What is a prestige, again?"],
+            correctAnswer: true
+        },
+    ],
+    responses:[
+        {
+            name: "response1 (This is an internal name, we won't have to use this value.)",
+            responses: [`Voila! Of course a Magician would never be caught without a rabbit in their hat!`, ` Other pets are fine for the less magically inclined, but a true magician always has a furry friend in his hat.`]
+        },
+        {
+            name: "response2 (This is an internal name, we won't have to use this value.)",
+            responses: [`A magician has many favorite colors. You have great taste, too!`, `Wow! You are so boring. A magician would never be caught dead in brown!`]
+        },
+        {
+            name: "response3 (This is an internal name, we won't have to use this value.)",
+            responses: [`Amazing! You are already performing magic! I love your scarf.`, `That is a sensible answer. However, it is not a very magical way to store your scarf.`]
+        },
+        {
+            name: "response4 (This is an internal name, we won't have to use this value.)",
+            responses: [`Abra kadabra! The wand is a perfect tool for wielding the magical elements. Impressive!`, `Ohhh... You seem to be pretty violent. Not exactly what we were looking for...`]
+        },
+        {
+            name: "response5 (This is an internal name, we won't have to use this value.)",
+            responses: [`Wow!! Thats right.`, `Not quite! A magician NEEDS to know every suit in a deck if they want to make cards appear from thin air!`]
+        },
+        {
+            name: "response6 (This is an internal name, we won't have to use this value.)",
+            responses: [`Very nice! Sounds like an excellent spot to wow an audience!`, `While you are relaxing, there is a whole world of people that is being left unimpressed. Shame on you.`]
+        },
+        {
+            name: "win/lose screen",
+            responses: [`HOW DID YOU DO THAT? Oh right, a magician never reveals their secret.`, `I'm sure your mother is very proud.`]
+        },
+
+    ],
+    result:[
+
+    ]
+};
